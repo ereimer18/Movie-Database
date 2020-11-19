05810c5e44429a0b340a322c79d75bfaf07ceec4
Ethan Reimer <ereimer18@georgefox.edu>
2020-11-19 03:15:43 -0800

    work on merging script

--- src/merge.py
+++ src/merge.py
@@ -8,8 +8,8 @@ def main():
     actor_csv = open('../final-csv/actor.csv')
     person_csv = open('../final-csv/people.csv')
     match_csv = open('../final-csv/matches.csv')
-    merge_handler = open('../final-csv/temp.csv', 'a')
-    act_csv = open('../final-csv/actor_w.csv', 'a')
+    merge_handler = open('../final-csv/temp_people.csv', 'a')
+    act_csv = open('../final-csv/temp_actor.csv', 'a')
     duplicate_csv = open('../final-csv/duplicates.csv', 'a')
 
     person_reader = csv.reader(person_csv, delimiter=',')
@@ -27,16 +27,15 @@ def main():
     actors = []
     people = []
     matches = []
+    pmatches = []
 
     for m in match_reader:
         name = m[0]
+        pname = m[1]
         matches.append(name)
+        pmatches.append(pname)
 
-    for person in person_reader:
-        if person_count > 0:
-            p_name = person[0] + person[1]
-            people.append(p_name)
-        person_count += 1
+
 
     for actor in actor_reader:
 
@@ -51,12 +50,28 @@ def main():
                     [actor[0], actor[1], actor[2], actor[3], actor[4], actor[5], actor[6],
                      actor[7], actor[8], actor[9], serial])
             else:
+                p = findPerson(a_name)
+                if p is not None:
+                    merge_writer.writerow(
+                        [p[0], p[1], p[2], p[3], p[4], p[5], p[6],p[7], serial])
+
                 actor_writer.writerow(
                     [actor[0], actor[1], actor[2], actor[3], actor[4], actor[5], actor[6],
                      actor[7], actor[8], actor[9], serial])
             serial += 1
 
         actor_count += 1
+
+    for person in person_reader:
+
+        if person_count > 0:
+            p_name = person[0] + person[1]
+            people.append(p_name)
+            if p_name not in pmatches:
+                merge_writer.writerow(
+                    [person[0], person[1], person[2], person[3], person[4], person[5], person[6], person[7], serial])
+        person_count += 1
+        serial += 1
 
     # match(people)
 
@@ -80,5 +95,23 @@ def match(people):
 def similar(a, b):
     return SequenceMatcher(None, a, b).ratio()
 
+
+def findPerson(a_name):
+    name = None
+    ret = None
+    match_csv = open('../final-csv/matches.csv')
+    match_reader = csv.reader(match_csv, delimiter=',')
+    for m in match_reader:
+        if a_name == m[0]:
+            name = m[1]
+
+    person_csv = open('../final-csv/people.csv')
+    person_reader = csv.reader(person_csv, delimiter=',')
+    for person in person_reader:
+        pName = person[0] + person[1]
+        if name == pName:
+            ret = person
+
+    return ret
 
 main()
